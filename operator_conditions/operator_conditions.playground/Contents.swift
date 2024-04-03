674fe246beb0942b0e9ac47420f43b2817f000ad
동영 <arhaxoda@naver.com>
2024-04-03 19:52:35 +0900

    operator part

---
+++ operator_conditions/operator_conditions.playground/Contents.swift
@@ -0,0 +1,53 @@
+import UIKit
+
+let first = 10
+let second = 3
+
+let sum = first + second
+let diff = first - second
+
+let multiple = first * second
+let divided = first / second
+
+let remainder = first % second
+
+
+let str1 = "aaa"
+let str2 = "bbb"
+
+let str = str1 + str2
+
+let name1 = ["april", "bob"]
+let name2 = ["chuck"]
+
+let name = name1 + name2
+
+
+var price1 = 10
+price1 += 1
+
+var price2 = 20
+price2 -= 5
+
+var price3 = 30
+price3 *= 3
+
+var price4 = 40
+price4 /= 2
+
+
+var quote = "Hello, Swift"
+quote += "Playgrounds"
+
+
+let score1 = 6
+let score2 = 4
+
+score1 == score2
+score1 != score2
+
+score1 > score2
+score1 >= score2
+
+score1 < score2
+score1 <= score2
