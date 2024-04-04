6bcfc281de5494231ccc0528194462d66060d0d0
동영 <arhaxoda@naver.com>
2024-04-04 18:57:30 +0900

    struct part 2 mutating

--- struct/struct.playground/Contents.swift
+++ struct/struct.playground/Contents.swift
@@ -55,3 +55,51 @@ task.progress = 50
 task.progress = 90
 task.progress = 100
 task.isDone
+
+
+struct Student {
+    var name: String
+    var major: String
+    var knowledge: Double
+    
+    func didFinalTest() -> Int {
+        let howMuchIdontKnow = (1 - knowledge) * 100
+        let score = 100 - Int(howMuchIdontKnow)
+        return score
+    }
+}
+
+var student = Student(name: "Jason", major: "CS", knowledge: 0.5)
+
+let score = student.didFinalTest()
+
+
+struct Student2 {
+    let name: String
+    var major: String
+    var knowledge: Double
+    
+    func didFinalTest() -> Int {
+        let howMuchIdontKnow = (1 - knowledge) * 100
+        let score = 100 - Int(howMuchIdontKnow)
+        return score
+    }
+    
+    mutating func didStudy() {
+        if knowledge >= 1 {
+            knowledge = 1
+        } else {
+            knowledge += 0.1
+        }
+    }
+}
+
+var student2 = Student2(name: "Jason", major: "CS", knowledge: 0.5)
+
+let score2 = student2.didFinalTest()
+
+student2.didStudy()
+student2.didStudy()
+
+let score3 = student2.didFinalTest()
+
