0b34cfe47df3a263788a69e95b0651afb9b9ac1b
동영 <arhaxoda@naver.com>
2024-04-22 01:37:52 +0900

    240422 컬렉션뷰 오토레이아웃

---
+++ StockRank/StockRank/StackRankViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StackRankViewController.swift
+//  StockRank
+//
+//  Created by Dahlia on 4/22/24.
+//
+
+import UIKit
+
+class StackRankViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
