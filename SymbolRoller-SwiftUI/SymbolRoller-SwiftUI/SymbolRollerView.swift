7b96b070c3d60ed8bcade426123ea97b5d54245c
동영 <arhaxoda@naver.com>
2024-07-22 23:53:10 +0900

    240722-2

--- SymbolRoller-SwiftUI/SymbolRoller-SwiftUI/SymbolRollerView.swift
+++ SymbolRoller-SwiftUI/SymbolRoller-SwiftUI/SymbolRollerView.swift
@@ -9,36 +9,41 @@ import SwiftUI
 
 struct SymbolRollerView: View {
 	
-	let symbols: [String] = ["sun.min","moon","cloud","wind","snowflake"]
+	let symbols: [String] = ["sun.min", "moon", "cloud", "wind", "snowflake"]
 	
-	@State var imageName: String = "moon"
+	@State var imageName: String = "moon"		// 상태 변경을 저장해야 하는 친구
 	
     var body: some View {
 		VStack {
+			Spacer()
+			
 			Image(systemName: imageName)
 				.renderingMode(.template)
 				.resizable()
 				.aspectRatio(contentMode: .fit)
 				.padding()
 			
+			Spacer()
+			
 			Text(imageName)
 				.font(.system(size: 40, weight: .bold))
 			
 			Button {
-				print("Button Tapped")
+				imageName = symbols.randomElement()!
 			} label: {
 				HStack {
 					Image(systemName: "arrow.3.trianglepath")
 					VStack {
 						Text("Reload")
-						Text("Reload")
+							.font(.system(size: 30, weight: .bold, design: .default))
+						Text("Click me to reload")
 					}
 				}
 			}
+			.foregroundStyle(.white)
 			.frame(maxWidth: .infinity, minHeight: 80)
 			.background(.pink)
 			.clipShape(Capsule())
-
 		}
 	}
 }
